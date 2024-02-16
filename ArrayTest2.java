a23ae5da2a784d9081ff7ac636804969a4c6eef0
seoyeon4087 <mollistemetum@naver.com>
2024-02-16 18:30:38 +0900

    240216 scores array

--- ArrayTest2.java
+++ ArrayTest2.java
@@ -7,28 +7,24 @@ public class ArrayTest2 {
         System.out.println("시험에 응시한 학생은 총 몇명입니까?");
         int student = sc.nextInt();
         int max = 0;
+        String first = "";
 
         String[] names = new String[student];
         int[] scores = new int[student];
 
         for (int i = 0; i < scores.length; i++) {
             System.out.println("시험에 응시한 학생의 이름은 무엇입니까?");
-            String name = sc.next();
-            names[i] = name;
-
+            names[i] = sc.next();
             System.out.println("점수를 입력하세요");
-            int score = sc.nextInt();
-            scores[i] = score;
-
-            System.out.println("이름과 점수 : " + name + " 학생 " + score + "점");
+            scores[i] = sc.nextInt();
 
             if (scores[i] > max) {
                 max = scores[i];
-
+                first = names[i];
 
             }
         }
 
-        System.out.println("1등은 " + names + ", " + max + "점이다");
+        System.out.println("1등은 " + first + ", " + max + "점이다");
     }
 }
