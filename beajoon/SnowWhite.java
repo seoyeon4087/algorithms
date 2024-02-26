bbbaf9f85e6f90c693b337edfd9c3656f3325b14
seoyeon4087 <mollistemetum@naver.com>
2024-02-26 18:12:07 +0900

    240226 lottoMatching

--- beajoon/SnowWhite.java
+++ beajoon/SnowWhite.java
@@ -1,48 +1,30 @@
 package beajoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SnowWhite {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int[] arr = {20, 7, 23, 19, 10, 15, 25, 8, 13};
-
-//        int[] arr = {sc.nextInt(), sc.nextInt(), sc.nextInt(),
-//                sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(),
-//                sc.nextInt(), sc.nextInt()};
-
+        int[] arr = new int[9];
         int sum = 0;
 
-        for (int i = 0; i < 9; i++) {
+        for(int i=0; i<9; i++){
+            arr[i] = sc.nextInt();
             sum += arr[i];
-            for(int j=0; j<8; j++){
-                if(sum - (arr[i] + arr[j]) == 100){
+        }
+
+        for(int i=0; i<8; i++){
+            for(int j=i+1; j<9; j++){
+                if(sum-(arr[i]+arr[j])==100){
                     arr[i] = 0;
                     arr[j] = 0;
-                } else {
-
+                    }
                 }
             }
-        }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
-                }
-            }
-        }
-
-//            for (int i = 0; i < arr.length; i++) {
-//                System.out.print(arr[i] + " ");
-//            }
-//            System.out.println();
-
-//        if(sum - (arr[i]+arr[j])==100){
-//            arr[i]=0;
-//            arr[j]=0;
-//        }
+        Arrays.sort(arr);
+        for(int k=2; k<9; k++){
+            System.out.println(arr[k]);
         }
     }
+}
--- matrixTest/LottoMatching.java
+++ matrixTest/LottoMatching.java
@@ -1,29 +1,117 @@
 package matrixTest;
 
 import java.util.Random;
-import java.util.Scanner;
 
 /**
- * 로또 발급은 1~8까지 중복없는 숫자 6개이다. 하나는 구매한 로또이다.
- * 추가된 로직은 추첨된 로또
- * 두 로또의 일치 여부에 다라 다음과 같은 메시지를 출력한다.
- * 단, 구매로또 번호와 추첨로또 번호 모두 같이 출력한다
- * <p>
- * 1등 = 6개 맞춤
- * 2등 = 5개
- * 3등 = 4개
- * 4등 = 3개
- * 꽝 = 2개 이하
- */
-
+ 1. 서로 겹치지 않는 숫자 6개를 생성
+ 2. 각 숫자는 1 ~ 45 범위 내의 숫자
+ 3. 매번 실행 시 다른 숫자 출력
+ 4. 오름차순 정렬
+ -> 랜덤수 : Math 사용
+ 결과)
+ ----------------
+ 로또 번호
+ ----------------
+ 2 11 25 27 35 38
+ * */
 public class LottoMatching {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("로또를 몇장 구매하시겠습니까?");
-        String num = sc.next(); // 로또 몇장 살건지
+        IBuyLotto buy = new BuyLotto();
+        ILottoDraw draw = new LottoDraw();
+        ILottoMatch match = new LottoMatch();
 
-//        String yourLotto = Lotto.main();
-//        System.out.println(yourLotto);
+        // 로또 구입
+
+        int[] arr = new int[6];
+        arr = draw.createArrayWithoutDuplicates(arr);
+        arr = draw.sortAscending(arr);
+        draw.printLotto(arr);
+
+        // 구입한 로또와 추첨한 로또의 일치여부 체크
 
     }
+
+
+}
+interface IBuyLotto{ int[] buyLotto();}
+interface ILottoDraw{
+    int createRandomNumber(int start, int end);
+    int[] createArrayWithoutDuplicates(int[] arr);
+    int[] sortAscending(int[] arr);
+    void printLotto(int[] arr);
+}
+interface ILottoMatch{
+    int findSame(int[] originLotto, int[] myLotto);
+    String rank(int count);
+}
+class BuyLotto implements IBuyLotto{
+
+    @Override
+    public int[] buyLotto() {
+        return new int[0];
+    }
+}
+class LottoDraw implements ILottoDraw{
+
+    @Override
+    public void printLotto(int[] arr) {
+        for(int i =0; i< arr.length; i++){
+            System.out.printf("%d \t", arr[i]);
+        }
+    }
+
+    @Override
+    public int[] sortAscending(int[] arr) {
+        // 버블 정렬이 들어갈 부분
+        for(int i=0; i< arr.length;i++){
+            for(int j=0; j < arr.length -1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    @Override
+    public int[] createArrayWithoutDuplicates(int[] arr) {
+        for(int i=0; i<6; i++){
+            int randomNumber = createRandomNumber(1,45);
+            boolean check = false;
+            for(int j=0; j<6; j++){
+                if(arr[j] == randomNumber){
+                    // 중복되면  check 를 true 로 바꿔라..
+                    check = true;
+                }
+            }
+            if(check==false){
+                arr[i] = randomNumber; // 중복되지 않았으니 배열에 담아라.
+            }else{
+                i--; // 중복됐으면 이번 회수는 무효로 하고 다시 뽑아라.
+            }
+
+        }
+        return arr;
+    }
+
+    @Override
+    public int createRandomNumber(int start, int end) {
+        Random random = new Random();
+        return random.nextInt(end)+start;
+    }
+
+}
+class LottoMatch implements ILottoMatch{
+    @Override
+    public int findSame(int[] originLotto, int[] myLotto){
+        return 0;
+    }
+
+    @Override
+    public String rank(int count) {
+        return null;
+    }
+
 }
