b09e1ad4bbeb7bc10b267569822ac2cb8c410e56
seoyeon4087 <mollistemetum@naver.com>
2024-02-28 19:45:26 +0900

    240228 meetingRoomMatrix

--- beajoon/MeetingRoom.java
+++ beajoon/MeetingRoom.java
@@ -1,43 +1,27 @@
 package beajoon;
 
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class MeetingRoom {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("회의 총 몇개?");
+        System.out.println("총 회의가 몇개?");
         int num = sc.nextInt();
+        System.out.println(num+"개");
 
-        int[] mae = new int[num];
-        int[] usi = new int[num];
-        int[] sum = new int[num];
+        int[][] meet = new int[num][2];
 
-        for (int i = 0; i < num; i++) {
-            mae[i] = sc.nextInt();
-            usi[i] = sc.nextInt();
+        for(int i=0;i<num;i++){
+            meet[i][0] = sc.nextInt();
+            meet[i][1] = sc.nextInt();
+
+            System.out.println();
+            System.out.println(meet[i][0] + " " + meet[i][1]);
         }
+//        Arrays.sort(meet);
 
-        for (int i = 0; i < num; i++) {
-            if (usi[0] > mae[i]) {
-                System.out.println(mae[0] + ", " + usi[0]);
 
-                if (sum[i] == sum[i + 1]) {
-                    System.out.println();
-                }
-
-            } else if (usi[i] > mae[i + 1]) {
-                System.out.println(mae[i] + ", " + usi[i]);
-                if (sum[i] == sum[i + 1]) {
-                    System.out.println();
-                }
-
-            } else {
-                System.out.println(mae[i + 1] + ", " + usi[i + 1]);
-                if (sum[i] == sum[i + 1]) {
-                    System.out.println();
-                }
-            }
-        }
     }
 }
